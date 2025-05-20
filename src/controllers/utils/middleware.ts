b9fb460f6c3441c0128ecf9ebb6a3c03c7aca3f4
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2025-05-20 17:31:19 +0700

    setup zod validate

---
+++ src/controllers/utils/middleware.ts
@@ -0,0 +1,34 @@
+import { z, ZodError } from "zod";
+import { Request, Response, NextFunction } from "express";
+import _ from "lodash"
+
+type Path = "body" | "query" | "params"
+
+export type SchemaType = z.ZodType<any, any, any>
+
+const handleError = (error: any) => {
+  if (error instanceof z.ZodError) {
+    return error.issues
+  }
+}
+
+export const Middlewares = {
+  validatorAsyncInput: <T extends SchemaType> (
+    schema: z.output<T>,
+    path: Path = "body",
+    isUpdate: Boolean = true
+  ) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const data = _.get(req, path)
+        const value = await schema.parseAsync(data)
+        if(isUpdate) {
+          req.body = value as (Record<string, unknown>)
+        }
+        next()
+      } catch (error) {
+        res.status(400).json({ error: handleError(error)})
+      }
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
-import express from "express";
+import express, { Router } from "express";
 import dotenv from "dotenv";
 import { prisma } from "./database/src/cilent"
+import bodyParser from 'body-parser';
 
+import testRoute from "./controllers/testzod/test.route"
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-  console.log("Res sent");
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/test", async (req, res) => {
-  try {
-    const created = await prisma.test.create({
-      data: {
-        name: "test"
-      }
-    })
+const router = Router();
+app.use(router);
 
-    res.status(201).json({ created })
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error)
-  }
-})
+router.use("/test", testRoute);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
