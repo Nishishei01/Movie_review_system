435e09d026f4b293ecbcc3fefbd15ceeb31f720a
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2025-06-18 12:25:54 +0700

    create crud api post

--- src/controllers/auth/auth.validator.ts
+++ src/controllers/auth/auth.validator.ts
@@ -7,7 +7,7 @@ const ValidateAuth = {
   jwtProtect: (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1]; 
-
+  
   if (!token) {
      res.status(401).json({ message: "Access token is missing" });
      return
---
+++ src/controllers/comment/comment.controller.ts
@@ -0,0 +1,89 @@
+import { Request, Response, NextFunction } from "express";
+import { Prisma } from "@prisma/client"
+import { prisma } from "../../database/src/cilent"
+import { UtilsHelpers } from "../../helpers/helper";
+import { ValidateCreateComment } from "./comment.validator";
+
+export default {
+  createComment: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { comment, postID, userID } = req.body as ValidateCreateComment
+      // const user = res.locals
+      // console.log(user)
+      
+      const result = await prisma.$transaction (async (tx) => {
+        const createdComment = await tx.comment.create({
+          data: {
+            comment,
+            postID,
+            userID
+          }
+        })
+        return createdComment
+      })
+      res.status(200).json({ message: "Create comment successfully.", result })
+    } catch (error) {
+      next(error)
+    }
+  },
+  // getAllComment: async (req: Request, res: Response, next: NextFunction) => {
+
+  // },
+  getByIdComment: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let queryList: Prisma.InputJsonValue[] = []
+
+      const { id } = req.params
+
+      if (id) {
+        queryList.push(
+          {
+            $match: {
+              _id: { $oid: id }
+            }
+          }
+        )
+      }
+      
+      const result = await prisma.comment.aggregateRaw({
+        pipeline: [
+          ...queryList
+        ]
+      })
+
+      UtilsHelpers.convertFieldList(result)
+
+      res.status(200).json({ result })
+    } catch (error) {
+      next(error)
+    }
+  },
+  updateComment: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+    const { id } = req.params
+    const { comment } = req.body
+    const result = await prisma.comment.update({
+        where: { id },
+        data: {
+          comment
+        }
+    })
+
+    res.status(200).json({ message: "Update successfully.", result })
+    } catch (error) {
+      next(error)
+    }
+  },
+  deleteComment: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const result = await prisma.comment.delete({
+        where: { id }
+      })
+
+      res.status(200).json({ message: "Delete successfully.", result})
+    } catch (error) {
+      next(error)
+    }
+  }
+}
