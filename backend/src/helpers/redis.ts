766cea56dc96ed36c8dd67589c2bc6e479ae2f5d
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2026-03-11 17:06:15 +0700

    fix redis disconnect

--- backend/src/helpers/redis.ts
+++ backend/src/helpers/redis.ts
@@ -1,7 +1,14 @@
 import { createClient } from 'redis';
 
 export const redisClient = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  pingInterval: 1000 * 60 * 3, //กำหนดเวลาให้มัน ping ทุกๆ 3 นาที กันมันตัดการเชื่อมต่อกัน
+  socket: {
+    reconnectStrategy: (retries) => {
+      console.log(`Reconnecting to Redis... Attempt ${retries}`);
+      return Math.min(retries * 1000, 5000); //ตั้งเวลาให้มัน reconnect ทุกๆ 1-5วิ
+    }
+  }
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
